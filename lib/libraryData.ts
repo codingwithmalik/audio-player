6d14f2e3a0ae44235a208c65aa5108cc98baea80
CodingWithMalik <codingwithmalik0@gmail.com>
2026-06-15 05:19:16 -0700

    There is some error relating to the previously used mock data as we are not using it now so I commented it out.

--- lib/libraryData.ts
+++ lib/libraryData.ts
@@ -1,66 +1,66 @@
-import { LibraryItem } from "@/features/LeftSidebar/Library/libraryTypes";
+// import { LibraryItem } from "@/features/LeftSidebar/Library/libraryTypes";
 
-export const libraryData: LibraryItem[] = [
-  {
-    id: "1",
-    type: "folder",
-    title: "Workout",
-    // icon: "/folder.png",
-    playlistsCount: 8,
-  },
+// export const libraryData: LibraryItem[] = [
+//   {
+//     id: "1",
+//     type: "folder",
+//     title: "Workout",
+//     // icon: "/folder.png",
+//     playlistsCount: 8,
+//   },
 
-  {
-    id: "2",
-    type: "playlist",
-    title: "GYM",
-    // coverImage: "/gym.jpg",
-    songsCount: 45,
-  },
+//   {
+//     id: "2",
+//     type: "playlist",
+//     title: "GYM",
+//     // coverImage: "/gym.jpg",
+//     songsCount: 45,
+//   },
 
-  {
-    id: "3",
-    type: "playlist",
-    title: "Jazz",
-    // coverImage: "/jazz.jpg",
-    songsCount: 80,
-  },
+//   {
+//     id: "3",
+//     type: "playlist",
+//     title: "Jazz",
+//     // coverImage: "/jazz.jpg",
+//     songsCount: 80,
+//   },
 
-  {
-    id: "4",
-    type: "folder",
-    title: "Coding",
-    // icon: "/folder.png",
-    playlistsCount: 5,
-  },
-  {
-    id: "5",
-    type: "folder",
-    title: "Yoga",
-    // icon: "/folder.png",
-    playlistsCount: 8,
-  },
+//   {
+//     id: "4",
+//     type: "folder",
+//     title: "Coding",
+//     // icon: "/folder.png",
+//     playlistsCount: 5,
+//   },
+//   {
+//     id: "5",
+//     type: "folder",
+//     title: "Yoga",
+//     // icon: "/folder.png",
+//     playlistsCount: 8,
+//   },
 
-  {
-    id: "6",
-    type: "playlist",
-    title: "Massage",
-    // coverImage: "/gym.jpg",
-    songsCount: 45,
-  },
+//   {
+//     id: "6",
+//     type: "playlist",
+//     title: "Massage",
+//     // coverImage: "/gym.jpg",
+//     songsCount: 45,
+//   },
 
-  {
-    id: "7",
-    type: "playlist",
-    title: "Sex",
-    // coverImage: "/jazz.jpg",
-    songsCount: 80,
-  },
+//   {
+//     id: "7",
+//     type: "playlist",
+//     title: "Sex",
+//     // coverImage: "/jazz.jpg",
+//     songsCount: 80,
+//   },
 
-  {
-    id: "8",
-    type: "folder",
-    title: "Coding",
-    // icon: "/folder.png",
-    playlistsCount: 5,
-  },
-];
+//   {
+//     id: "8",
+//     type: "folder",
+//     title: "Coding",
+//     // icon: "/folder.png",
+//     playlistsCount: 5,
+//   },
+// ];
